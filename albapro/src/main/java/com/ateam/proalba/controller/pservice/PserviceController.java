4a3a335f7f108f36bfed44fbf98fcbbb990e8bfd
Nangman <ckdgh930314@naver.com>
2019-04-17 18:39:21 +0900

    등록일 오류 수정~

--- albapro/src/main/java/com/ateam/proalba/controller/pservice/PserviceController.java
+++ albapro/src/main/java/com/ateam/proalba/controller/pservice/PserviceController.java
@@ -27,21 +27,21 @@ public class PserviceController {
 	@RequestMapping("/pservice")
 	public String pservice(Model model) {
 		logger.info("Welcome PservicePage");
-		model.addAttribute("message", "媛쒖씤�꽌鍮꾩뒪 �럹�씠吏� 諛⑸Ц�쓣 �솚�쁺�빀�땲�떎");
+		model.addAttribute("message", "");
 		return "pservice/pservice";
 	}
 	
 	@RequestMapping(value = "/writeResume", method = RequestMethod.GET)
 	public String wresumeGET(Model model) throws Exception {
 		logger.info("Welcome wresumePage");
-		model.addAttribute("message", "wresumePage �럹�씠吏� 諛⑸Ц�쓣 �솚�쁺�빀�땲�떎");
+		model.addAttribute("message", "wresumePage");
 		return "pservice/writeResume";
 	}
 	
 	@RequestMapping(value = "/maresume", method = RequestMethod.GET)
 	public String maresumeGET(Model model) throws Exception {
 		logger.info("Welcome maresumePage");
-		model.addAttribute("message", "maresumePage �럹�씠吏� 諛⑸Ц�쓣 �솚�쁺�빀�땲�떎");
+		model.addAttribute("message", "maresumePage");
 		return "pservice/maresume";
 	}
 	
@@ -62,21 +62,21 @@ public class PserviceController {
 	@RequestMapping(value = "/ecertifi", method = RequestMethod.GET)
 	public String ecertifiGET(Model model) throws Exception {
 		logger.info("Welcome ecertifiPage");
-		model.addAttribute("message", "ecertifiPage �럹�씠吏� 諛⑸Ц�쓣 �솚�쁺�빀�땲�떎");
+		model.addAttribute("message", "ecertifiPage");
 		return "pservice/ecertifi";
 	}
 	
 	@RequestMapping(value = "/pworkmanage", method = RequestMethod.GET)
 	public String workmanageGET(Model model) throws Exception {
 		logger.info("Welcome workmanagePage");
-		model.addAttribute("message", "workmanagePage �럹�씠吏� 諛⑸Ц�쓣 �솚�쁺�빀�땲�떎");
+		model.addAttribute("message", "workmanagePage");
 		return "pservice/pworkmanage";
 	}
 	
 	@RequestMapping(value = "/inqsalary", method = RequestMethod.GET)
 	public String inqsalaryGET(Model model) throws Exception {
 		logger.info("Welcome inqsalaryPage");
-		model.addAttribute("message", "inqsalaryPage �럹�씠吏� 諛⑸Ц�쓣 �솚�쁺�빀�땲�떎");
+		model.addAttribute("message", "inqsalaryPage");
 		return "pservice/inqsalary";
 	}
 	
@@ -100,7 +100,7 @@ public class PserviceController {
 	@RequestMapping(value = "/employmentinfo", method = RequestMethod.GET)
 	public String employmentinfoGET(Model model) throws Exception {
 		logger.info("Welcome inqcareerPage");
-		model.addAttribute("message", "inqcareerPage �럹�씠吏� 諛⑸Ц�쓣 �솚�쁺�빀�땲�떎");
+		model.addAttribute("message", "inqcareerPage");
 		return "pservice/employmentinfo";
 	}
 	@RequestMapping(value = "/contract", method = RequestMethod.GET)
