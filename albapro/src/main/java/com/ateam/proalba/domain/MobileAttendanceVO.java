3ab36d98fc720941232dbb01e797babcfdd7576d
KImJinSu <45416779+wdamanghara@users.noreply.github.com>
2019-05-08 21:16:44 +0900

    모바일 메인 카드 컨트롤러 완료 근태 db 값가져오기 가능 씹가능

---
+++ albapro/src/main/java/com/ateam/proalba/domain/MobileAttendanceVO.java
@@ -0,0 +1,15 @@
+package com.ateam.proalba.domain;
+
+
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class MobileAttendanceVO {
+	private Date work_strt_time;
+	private String attendance_status;
+	private String sa_code;
+
+}
