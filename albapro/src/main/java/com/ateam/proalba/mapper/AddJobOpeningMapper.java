5ad0003e6c3fe855087c50c9e45e01fb5fcaeb48
Nnagman <ckdgh930314@naver.com>
2019-05-21 10:24:46 +0900

    훠훠훠 경력부분 수정함

--- albapro/src/main/java/com/ateam/proalba/mapper/AddJobOpeningMapper.java
+++
@@ -1,7 +0,0 @@
-package com.ateam.proalba.mapper;
-
-import com.ateam.proalba.domain.NoticeVO;
-
-public interface AddJobOpeningMapper {
-	public void addJobOpening(NoticeVO noticeVO) throws Exception;
-}
