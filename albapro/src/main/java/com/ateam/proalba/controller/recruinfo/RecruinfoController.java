899d3f9b8f2fa6270cc613f362239ed4d3f795dc
rkd2849 <arkd2849@gmail.com>
2019-07-08 11:21:17 +0900

    123

--- albapro/src/main/java/com/ateam/proalba/controller/recruinfo/RecruinfoController.java
+++ albapro/src/main/java/com/ateam/proalba/controller/recruinfo/RecruinfoController.java
@@ -2,6 +2,7 @@ package com.ateam.proalba.controller.recruinfo;
 
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -11,11 +12,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.ateam.proalba.domain.CareerVO;
 import com.ateam.proalba.domain.NoticeVO;
+import com.ateam.proalba.domain.ResumeVO;
+import com.ateam.proalba.service.CareerService;
+import com.ateam.proalba.service.ResumeService;
 import com.ateam.proalba.service.recruinfo.RecruinfoService;
 
 @Controller
